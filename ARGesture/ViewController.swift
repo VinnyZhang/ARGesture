eb3a988f9ce94f95fa17a11e1a027ae2511bb540
VinnyZhang <848967151@qq.com>
2018-04-26 19:30:26 +0800

    添加对SCNView的基本处理

--- ARGesture/ViewController.swift
+++ ARGesture/ViewController.swift
@@ -26,7 +26,15 @@ class ViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupConfig()
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        scnView.session.pause()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -34,6 +42,7 @@ class ViewController: UIViewController {
     }
     
     
+    /// 初始化子视图
     func layoutMySubItems()
     {
         scnView.frame = self.view.frame
@@ -41,7 +50,7 @@ class ViewController: UIViewController {
         scnView.delegate = self
         scnView.showsStatistics = true
         scnView.autoenablesDefaultLighting = true
-        scnView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,ARSCNDebugOptions.showFeaturePoints]
+        scnView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         maskView.frame = self.view.frame
         maskView.backgroundColor = UIColor.white
@@ -54,6 +63,75 @@ class ViewController: UIViewController {
         tipLabel.numberOfLines = 0
         
     }
+    
+    /// 添加配置
+    func setupConfig() {
+        if ARWorldTrackingConfiguration.isSupported {//判断是否支持6个自由度
+            let worldTracking = ARWorldTrackingConfiguration()//6DOF【3个旋转轴 3个平移轴】
+            worldTracking.planeDetection = .horizontal
+            worldTracking.isLightEstimationEnabled = true
+            sessionConfig = worldTracking
+        }
+        else {
+            let orientationTracking = AROrientationTrackingConfiguration()//3DOF 【3个旋转轴】
+            sessionConfig = orientationTracking
+            tipLabel.text = "当前设备不支持6DOF跟踪"
+        }
+        scnView.session.run(sessionConfig!)
+    }
+    
+    /// 添加手势
+    func addGestureOfScnView() {
+//        添加单击手势
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureEventFrom(tapGestureRecognizer:)))
+        tapGesture.numberOfTapsRequired = 1
+        self.scnView.addGestureRecognizer(tapGesture)
+        
+//        添加长按手势
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(longGestureEventFrom(longGestureRecognizer:)))
+        longGesture.minimumPressDuration = 1
+        self.scnView.addGestureRecognizer(longGesture)
+        
+//        添加华滑动手势
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureEventFrom(panGestureRecognizer:)))
+        panGesture.maximumNumberOfTouches = 1
+        self.scnView.addGestureRecognizer(panGesture)
+        
+//        添加捏合手势
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(pinchGestureEventFrom(pinchGestureRecognizer:)))
+        self.scnView.addGestureRecognizer(pinchGesture)
+        
+        
+    }
+    
+    
+    /// 单击手势触发动作
+    ///
+    /// - Parameter tapGestureRecongizer: 手势触发信息
+    @objc func tapGestureEventFrom(tapGestureRecognizer : UITapGestureRecognizer) {
+        
+    }
+    
+    /// 长按手势触发动作
+    ///
+    /// - Parameter longGestureRecognizer: 手势触发信息
+    @objc func longGestureEventFrom(longGestureRecognizer : UILongPressGestureRecognizer) {
+        
+    }
+    
+    /// 滑动手势触发动作
+    ///
+    /// - Parameter panGestureRecognizer: 滑动手势触发信息
+    @objc func panGestureEventFrom(panGestureRecognizer: UIPanGestureRecognizer) {
+        
+    }
+    
+    /// 捏合手势触发动作
+    ///
+    /// - Parameter pinchGestureRecognizer: 捏合手势信息
+    @objc func pinchGestureEventFrom(pinchGestureRecognizer: UIPinchGestureRecognizer) {
+        
+    }
 
 
 }
@@ -61,6 +139,158 @@ class ViewController: UIViewController {
 
 // MARK: - 代理方法
 extension ViewController: ARSCNViewDelegate {
+    //相机状态变化
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        //判断状态
+        switch camera.trackingState{
+        case .notAvailable:
+            tipLabel.text = "跟踪不可用"
+            UIView.animate(withDuration: 0.5) {
+                self.maskView.alpha = 0.7
+            }
+        case .limited(ARCamera.TrackingState.Reason.initializing):
+            let title = "有限的跟踪，原因是："
+            let desc = "正在初始化，请稍后"
+            tipLabel.text = title + desc
+            UIView.animate(withDuration: 0.5) {
+                self.maskView.alpha = 0.6
+            }
+        case .limited(ARCamera.TrackingState.Reason.relocalizing):
+            tipLabel.text = "有限的跟踪，原因是：重新初始化"
+            UIView.animate(withDuration: 0.5) {
+                self.maskView.alpha = 0.6
+            }
+        case .limited(ARCamera.TrackingState.Reason.excessiveMotion):
+            tipLabel.text = "有限的跟踪，原因是：设备移动过快请注意"
+            UIView.animate(withDuration: 0.5) {
+                self.maskView.alpha = 0.6
+            }
+        case .limited(ARCamera.TrackingState.Reason.insufficientFeatures):
+            tipLabel.text = "有限的跟踪，原因是：提取不到足够的特征点，请移动设备"
+            UIView.animate(withDuration: 0.5) {
+                self.maskView.alpha = 0.6
+            }
+        case .normal:
+            tipLabel.text = "跟踪正常"
+            UIView.animate(withDuration: 0.5) {
+                self.maskView.alpha = 0.0
+            }
+        }
+    }
+    
+    //会话被中断
+    func sessionWasInterrupted()
+    {
+        
+        tipLabel.text = "会话中断"
+    }
+    
+    //会话中断结束
+    func sessionInterruptionEnded(_ session: ARSession) {
+        tipLabel.text = "会话中断结束，已重置会话"
+        scnView.session.run(self.sessionConfig!, options: .resetTracking)
+    }
+    
+    
+    //会话失败
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        print(error.localizedDescription)
+        tipLabel.text  = error.localizedDescription
+    }
+    
+    /**
+     实现此方法来为给定 anchor 提供自定义 node。
+     
+     @discussion 此 node 会被自动添加到 scene graph 中。
+     如果没有实现此方法，则会自动创建 node。
+     如果返回 nil，则会忽略此 anchor。
+     @param renderer 将会用于渲染 scene 的 renderer。
+     @param anchor 新添加的 anchor。
+     @return 将会映射到 anchor 的 node 或 nil。
+     */
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let anchor = anchor as? ARPlaneAnchor else {
+            return nil
+        }
+        
+        let node = PlaneNode(withAnchor: anchor)
+        
+        
+        DispatchQueue.main.async(execute: {
+            self.tipLabel.text = "检测到平面并已添加到场景中，点击屏幕可刷新会话"
+        })
+        
+        return node
+        
+    }
+    
+    /**
+     将新 node 映射到给定 anchor 时调用。
+     
+     @param renderer 将会用于渲染 scene 的 renderer。
+     @param node 映射到 anchor 的 node。
+     @param anchor 新添加的 anchor。
+     */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        
+        
+    }
+    
+    /**
+     将要用给定 anchor 的数据来更新时 node 调用。
+     
+     @param renderer 将会用于渲染 scene 的 renderer。
+     @param node 即将更新的 node。
+     @param anchor 被更新的 anchor。
+     */
+    func renderer(_ renderer: SCNSceneRenderer, willUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let anchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        guard let node = node as? PlaneNode else {
+            return
+        }
+        node.updatePlane(anchor: anchor)
+        DispatchQueue.main.async(execute: {
+            self.tipLabel.text = "场景内有平面更新"
+            self.tipLabel.text = "\(anchor.center.x) , \(anchor.center.y) ,\(anchor.center.z)"
+        })
+    }
+    
+    /**
+     使用给定 anchor 的数据更新 node 时调用。
+     
+     @param renderer 将会用于渲染 scene 的 renderer。
+     @param node 更新后的 node。
+     @param anchor 更新后的 anchor。
+     */
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+    
+    /**
+     从 scene graph 中移除与给定 anchor 映射的 node 时调用。
+     
+     @param renderer 将会用于渲染 scene 的 renderer。
+     @param node 被移除的 node。
+     @param anchor 被移除的 anchor。
+     */
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let anchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        guard let node = node as? PlaneNode else {
+            return
+        }
+        node.removePlaneNode(WithAnchor: anchor)
+        
+        DispatchQueue.main.async(execute: {
+            self.tipLabel.text = "场景内有平面被删除"
+        })
+        
+    }
     
 }
 
